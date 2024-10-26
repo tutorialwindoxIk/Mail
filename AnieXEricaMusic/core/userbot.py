aa7ba6ff0f0a3d28c7538d10352de67816889dfa
manoranjan23 <manoranjanhor43@gmail.com>
2024-10-26 10:54:43 +0530

    Update userbot.py

--- AnieXEricaMusic/core/userbot.py
+++ AnieXEricaMusic/core/userbot.py
@@ -54,9 +54,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AbhiModszYT_Return")
-                await self.one.join_chat("AmBotYT")
-                await self.one.join_chat("SuperBanSBots")
+                await self.one.join_chat("somueditingzone")
+                await self.one.join_chat("somueditingzone")
+                await self.one.join_chat("somueditingzone")
             except:
                 pass
             assistants.append(1)
@@ -71,8 +71,8 @@ class Userbot(Client):
             self.one.name = (await self.one.get_me()).mention
             self.one.username = (await self.one.get_me()).username
             assistantids.append(self.one.id)
-            ambots = "@MineROBOT"
-            gupta = "@Gupta_876bot"
+            ambots = "@ll_SOMU_xd_ll_BOT"
+            gupta = "@ll_ishu_xd_ll_bot"
             try:
                 await self.one.send_message(ambots, f"/start")
                 await self.one.send_message(gupta, f"/start")
@@ -89,9 +89,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("AbhiModszYT_Return")
-                await self.two.join_chat("AmBotYT")
-                await self.two.join_chat("SuperBanSBots")
+                await self.two.join_chat("somueditingzone")
+                await self.two.join_chat("somueditingzone")
+                await self.two.join_chat("somueditingzone")
             except:
                 pass
             assistants.append(2)
@@ -111,9 +111,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("AbhiModszYT_Return")
-                await self.three.join_chat("AmBotYT")
-                await self.three.join_chat("SuperBanSBots")
+                await self.three.join_chat("somueditingzone")
+                await self.three.join_chat("somueditingzone")
+                await self.three.join_chat("somueditingzone")
             except:
                 pass
             assistants.append(3)
@@ -133,9 +133,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("AbhiModszYT_Return")
-                await self.four.join_chat("AmBotYT")
-                await self.four.join_chat("SuperBanSBots")
+                await self.four.join_chat("somueditingzone")
+                await self.four.join_chat("somueditingzone")
+                await self.four.join_chat("somueditingzone")
             except:
                 pass
             assistants.append(4)
@@ -155,9 +155,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("AbhiModszYT_Return")
-                await self.five.join_chat("AmBotYT")
-                await self.five.join_chat("SuperBanSBots")
+                await self.five.join_chat("somueditingzone")
+                await self.five.join_chat("somueditingzone")
+                await self.five.join_chat("somueditingzone")
             except:
                 pass
             assistants.append(5)
