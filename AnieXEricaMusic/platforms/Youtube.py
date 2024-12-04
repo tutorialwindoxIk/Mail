e5174bc7d59b635f7cb415a0bd7e2566c52c2709
manoranjan23 <manoranjanhor43@gmail.com>
2024-12-04 18:00:31 +0530

    Update Youtube.py

--- AnieXEricaMusic/platforms/Youtube.py
+++ AnieXEricaMusic/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from DAXXMUSIC.utils.database import is_on_off
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from AnieXEricaMusic.utils.database import is_on_off
+from AnieXEricaMusic.utils.formatters import time_to_seconds
 
 
 
