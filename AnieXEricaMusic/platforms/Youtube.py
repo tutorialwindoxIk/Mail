fe29c03412b0b90eae8aad1a77fb14d2d040d5b3
manoranjan23 <manoranjanhor43@gmail.com>
2024-12-08 15:53:21 +0530

    Update Youtube.py

--- AnieXEricaMusic/platforms/Youtube.py
+++ AnieXEricaMusic/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnieXEricaMusic.utils.database import is_on_off
-from AnieXEricaMusic.utils.formatters import time_to_seconds
+from SlayerX.utils.database import is_on_off
+from SlayerX.utils.formatters import time_to_seconds
 
 
 
