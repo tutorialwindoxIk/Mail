c45a189c83330f606e3ae0d4a45ac51c2f2c2ce6
Ishu_official <somnathhor12052005@gmail.com>
2025-04-27 14:07:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1002461188676))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1002100433415))
 
 OWNER_ID = int(getenv("OWNER_ID", 6908972904))
 
